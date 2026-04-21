e31117b3e68fce7a1c9b490c5416c808ea0eb67a
jamalabbay97 <jamalabbay97@example.com>
2026-04-21 15:13:10 +0100

    Update VentilationItem with category and general UI/localization improvements

--- lib/data/repositories/report_repository_impl.dart
+++ lib/data/repositories/report_repository_impl.dart
@@ -1,39 +1,61 @@
 import 'package:r0/domain/models/report.dart';
 import 'package:r0/domain/repositories/report_repository.dart';
 import 'package:r0/data/services/database_helper.dart';
+import 'package:r0/data/services/firestore_service.dart';
+import 'package:r0/data/services/sync_service.dart';
 
 class ReportRepositoryImpl implements ReportRepository {
   final DatabaseHelper _databaseHelper;
+  final SyncService _syncService = SyncService();
+  final FirestoreService _firestoreService = FirestoreService();
 
   ReportRepositoryImpl(this._databaseHelper);
 
+  Future<void> _syncIfAuthenticated() async {
+    if (_firestoreService.isAuthenticated) {
+      await _syncService.performFullSync();
+    }
+  }
+
   @override
-  Future<int> insertReport(Report report) {
+  Future<int> insertReport(Report report) async {
+    if (_firestoreService.isAuthenticated) {
+      return _syncService.saveReport(report);
+    }
+
     return _databaseHelper.insertReport(report);
   }
 
   @override
-  Future<List<Report>> getReports() {
+  Future<List<Report>> getReports() async {
+    await _syncIfAuthenticated();
     return _databaseHelper.getReports();
   }
 
   @override
-  Future<List<Report>> getReportsPage({int limit = 50, int offset = 0}) {
+  Future<List<Report>> getReportsPage({int limit = 50, int offset = 0}) async {
+    await _syncIfAuthenticated();
     return _databaseHelper.getReportsPage(limit: limit, offset: offset);
   }
 
   @override
-  Future<List<Report>> getReportsByType(String type) {
+  Future<List<Report>> getReportsByType(String type) async {
+    await _syncIfAuthenticated();
     return _databaseHelper.getReportsByType(type);
   }
 
   @override
-  Future<Report?> getReport(int id) {
+  Future<Report?> getReport(int id) async {
+    await _syncIfAuthenticated();
     return _databaseHelper.getReport(id);
   }
 
   @override
-  Future<int> updateReport(Report report) {
+  Future<int> updateReport(Report report) async {
+    if (_firestoreService.isAuthenticated) {
+      await _syncService.updateReport(report);
+      return 1;
+    }
     return _databaseHelper.updateReport(report);
   }
 
